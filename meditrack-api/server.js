3a040f8ca6aa34c1358b30a6b7f0c27144fc307e
sclau <saftaclaudia27@yahoo.com>
2026-08-08 20:16:27 +0300

    Add Vervel domain to CORS

--- meditrack-api/server.js
+++ meditrack-api/server.js
@@ -16,7 +16,12 @@ const mealTemplateRoutes = require("./routes/mealTemplateRoutes");
 
 // Cron jobs
 const { scheduleExamNotifications } = require("./jobs/examNotifications");
-const { scheduleCaloriesReminder, scheduleCaloriePacing, scheduleWaterReminders, scheduleMealReminders } = require("./jobs/caloriesNotifications");
+const {
+  scheduleCaloriesReminder,
+  scheduleCaloriePacing,
+  scheduleWaterReminders,
+  scheduleMealReminders,
+} = require("./jobs/caloriesNotifications");
 
 const app = express();
 
@@ -24,7 +29,17 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "https://meditrack-app-aps1.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
