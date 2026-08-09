b7c1a101a6e7ff781a8a600e1a0ae779c9cda2a3
sclau <saftaclaudia27@yahoo.com>
2026-08-09 15:06:40 +0300

    fix:add correct Vervel origin to CORS whitelist

--- meditrack-api/server.js
+++ meditrack-api/server.js
@@ -36,6 +36,7 @@ app.use(
       "http://localhost:5174",
       "http://localhost:5175",
       "https://meditrack-app-aps1.vercel.app",
+      "https://meditrack-app-six.vercel.app",
     ],
     credentials: true,
   }),
